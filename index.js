bb11ddc0b96be3ec4d21d18102dca19f01e975ed
ludoblues <ludovic.vue@gmail.com>
2015-07-28 15:36:47 +0200

    add port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const child_process = require('child_process');
 const numCPUs = require('os').cpus().length;
 
 const core = {
-	'wac-front': [ child_process.fork('./node_modules/@ludoblues/front'), child_process.fork('./node_modules/@ludoblues/front') ],
+	'wac-front': [ child_process.fork('./node_modules/@ludoblues/front', [ '2000' ]), child_process.fork('./node_modules/@ludoblues/front', [ '2001' ]) ],
 	'wac-contest': [ child_process.fork('./node_modules/@ludoblues/contest') ],
 	'wac-jobs': [ child_process.fork('./node_modules/@ludoblues/jobs') ]
 };
