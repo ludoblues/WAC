b89a6312f7acc8f6a8ce121adef962bd71c63125
Ludovic <ludovic.vue@gmail.com>
2015-07-28 15:07:18 +0200

    git branches

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const child_process = require('child_process');
 const numCPUs = require('os').cpus().length;
 
 const core = {
-	'wac-front': [ child_process.fork('./node_modules/wac-front'), child_process.fork('./node_modules/wac-front') ],
-	'wac-contest': [ child_process.fork('./node_modules/wac-contest') ],
-	'wac-jobs': [ child_process.fork('./node_modules/wac-jobs') ]
+	'wac-front': [ child_process.fork('./node_modules/@ludoblues/front'), child_process.fork('./node_modules/@ludoblues/front') ],
+	'wac-contest': [ child_process.fork('./node_modules/@ludoblues/contest') ],
+	'wac-jobs': [ child_process.fork('./node_modules/@ludoblues/jobs') ]
 };
