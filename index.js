fe37a2363deef903beb7eb20898ac9d2d9f7c302
ludoblues <ludovic.vue@gmail.com>
2015-07-29 17:23:21 +0200

    add cron module

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 const child_process = require('child_process');
 const numCPUs = require('os').cpus().length;
+const CronJob = require('cron').CronJob;
 
 const core = {
 	'wac-front': [ child_process.fork('./node_modules/@ludoblues/front', [ '2000' ]), child_process.fork('./node_modules/@ludoblues/front', [ '2001' ]) ],
 	'wac-contest': [ child_process.fork('./node_modules/@ludoblues/contest') ],
 	'wac-jobs': [ child_process.fork('./node_modules/@ludoblues/jobs') ]
 };
+
+const job = new CronJob('0 0 0 * * *', function() { core['wac-contest'][0].send({ event: 'end-of-the-day' }); });
+job.start();
