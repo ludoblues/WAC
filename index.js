4b2a21d3a1c58290c6ddad921fc97ad16e590613
ludoblues <ludovic.vue@gmail.com>
2015-09-04 22:03:41 +0200

    add cluster

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const child_process = require('child_process');
+const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const CronJob = require('cron').CronJob;
 
+cluster.setupMaster({ exec: './node_modules/@ludoblues/front' });
+cluster.fork();
+cluster.fork();
+
 const core = {
-	'wac-front': [ child_process.fork('./node_modules/@ludoblues/front', [ '2000' ]), child_process.fork('./node_modules/@ludoblues/front', [ '2001' ]) ],
 	'wac-contest': [ child_process.fork('./node_modules/@ludoblues/contest') ],
 	'wac-jobs': [ child_process.fork('./node_modules/@ludoblues/jobs') ]
 };
